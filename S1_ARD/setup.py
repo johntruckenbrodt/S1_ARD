40fdde086fc79e829dc9a8ddfed1f2125ce41ba4
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2018-12-17 18:48:00 +0100

    [setup.py] initial commit

---
+++ S1_ARD/setup.py
@@ -0,0 +1,39 @@
+import os
+import sys
+from setuptools import setup, find_packages
+
+directory = os.path.abspath(os.path.dirname(__file__))
+if sys.version_info >= (3, 0):
+    with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+        long_description = f.read()
+else:
+    with open(os.path.join(directory, 'README.md')) as f:
+        long_description = f.read()
+
+setup(name='S1_ARD',
+      packages=find_packages(),
+      include_package_data=True,
+      version='0.1',
+      description='A common workspace for our publication in the MDPI Data special issue on data cubes',
+      classifiers=[
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX :: Linux',
+          'Programming Language :: Python',
+      ],
+      install_requires=['numpy',
+                        'matplotlib',
+                        'pandas',
+                        'scipy',
+                        'jupyter',
+                        'IPython',
+                        'ipywidgets',
+                        'scikit-learn'],
+      extras_require={
+          'docs': ['sphinx'],
+      },
+      url='https://github.com/johntruckenbrodt/S1_ARD',
+      author='John Truckenbrodt',
+      author_email='john.truckenbrodt@uni-jena.de',
+      zip_safe=False,
+      long_description=long_description,
+      long_description_content_type='text/markdown')
