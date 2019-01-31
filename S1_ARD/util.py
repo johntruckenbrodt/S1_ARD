[dem_aspect] fixed bugs in computation

S1_ARD/util.py
@@ -16,6 +16,7 @@ from matplotlib.offsetbox import AnchoredText
 from osgeo import gdal
 from spatialist import Raster
 
+
 # Function to generate one to one plots for each land cover class
 # from specified images. (Utilises function one.)
 def one2oneSARplots(in_sar1_img, in_sar2_img, band, sar1_ref, sar2_ref, lc, lc_ints, lc_string):
@@ -203,22 +204,21 @@ def sar_vs_inc(file_sar, file_inc, nsamples, nodata=-99, db_convert=False, title
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-
+    
     # set axes range
     bottom, top = plt.ylim()
     plt.ylim(ymin if ymin is not None else bottom, ymax if ymax is not None else ymax)
 
 
 def dem_aspect(img):
-    
     kernel = CustomKernel(np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]) / 8.)
-    xchangerate_array = convolve(img, kernel, normalize_kernel=False)
+    xchangerate_array = -convolve(img, kernel, normalize_kernel=False)
     
     kernel = CustomKernel(np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]]) / 8.)
-    ychangerate_array = convolve(img, kernel, normalize_kernel=False)
+    ychangerate_array = -convolve(img, kernel, normalize_kernel=False)
     
-    aspect = 180. / np.pi * np.arctan(ychangerate_array, -xchangerate_array)
-    aspect_value = aspect
+    aspect = np.rad2deg(np.arctan2(ychangerate_array, -xchangerate_array))
+    aspect_value = np.copy(aspect)
     
     mask = aspect < 0.0
     aspect_value[mask] = 90.0 - aspect[mask]
@@ -232,7 +232,6 @@ def dem_aspect(img):
 
 
 def dem_slope(img, x_cell_size, y_cell_size):
-    
     kernel = CustomKernel(np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]]) / (8. * x_cell_size))
     xchangerate_array = convolve(img, kernel, normalize_kernel=False)
     
@@ -277,10 +276,14 @@ def visible_sar_angle_map(head_angle, inc_angle, look_dir='right'):
                        np.cos(radius)])
     
     # condition that facet is visible by radar
-    view_prod = normal[0, :, :] * look_vec[0] + normal[1, :, :] * look_vec[1] + normal[2, :, :] * look_vec[2]
+    view_prod = normal[0, :, :] * look_vec[0] + \
+                normal[1, :, :] * look_vec[1] + \
+                normal[2, :, :] * look_vec[2]
     
     # condition that facet is tilted positive in viewplane
-    mont_prod = normal[0, :, :] * viewplane[0] + normal[1, :, :] * viewplane[1] + normal[2, :, :] * viewplane[2]
+    mont_prod = normal[0, :, :] * viewplane[0] + \
+                normal[1, :, :] * viewplane[1] + \
+                normal[2, :, :] * viewplane[2]
     
     # visible map_array
     visible_map_array = (view_prod < 0) & (mont_prod > 0)