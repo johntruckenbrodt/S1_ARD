d9835cf3b88acda9bf133dd517f1697957bdacdc
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2019-04-04 10:29:05 +0200

    [__init__] renamed function one2oneSARplots to scatter

--- S1_ARD/__init__.py
+++ S1_ARD/__init__.py
@@ -1,2 +1,2 @@
-from .util import one2oneSARplots, one2oneSARplotsratio, readimg2array, simplify_lc, sar_vs_inc, \
+from .util import scatter, one2oneSARplotsratio, readimg2array, simplify_lc, sar_vs_inc, \
     dem_aspect, dem_slope, visible_sar_angle_map, dem_distribution
