e0102e875ebc80ae817c87be496d096d21c7ef66
John Truckenbrodt <john.truckenbrodt@uni-jena.de>
2019-04-14 16:57:53 +0200

    [__init__] removed import of function sar_vs_inc

--- S1_ARD/__init__.py
+++ S1_ARD/__init__.py
@@ -1,2 +1,2 @@
-from .util import scatter, one2oneSARplotsratio, readimg2array, simplify_lc, sar_vs_inc, \
+from .util import scatter, one2oneSARplotsratio, readimg2array, simplify_lc, \
     dem_aspect, dem_slope, visible_sar_angle_map, dem_distribution
